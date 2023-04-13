724197e422c44e618a80539da5e25473358c1e83
blustro <brunolustro@gmail.com>
2023-04-13 18:27:48 +0100

    fix(root): removed log

--- packages/date-logic-dunga/index.js
+++ packages/date-logic-dunga/index.js
@@ -2,6 +2,5 @@ module.exports = function getDate(
     locale = 'en-US',
     options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 ) {
-    console.log('testing');
     return new Date().toLocaleDateString(locale, options);
 };
