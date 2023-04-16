4b8a8e3ce9755ed6cd98d66cfb187aa55c24f7a7
Bruno Lustro <brunolustro@gmail.com>
2023-04-16 17:53:46 +0100

    why not work?

--- packages/date-logic-dunga/index.js
+++ packages/date-logic-dunga/index.js
@@ -2,6 +2,6 @@ module.exports = function getDate(
   locale = "en-US",
   options = { weekday: "long", year: "numeric", month: "long", day: "numeric" }
 ) {
-  console.log("2");
+  console.log("3");
   return new Date().toLocaleDateString(locale, options);
 };
